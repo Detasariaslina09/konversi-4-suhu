4e36a6a221aef0abe06ac2f243014ac601375fd4
DETA SARI ASLINA <112565733+Detasariaslina09@users.noreply.github.com>
2022-11-02 17:53:49 +0700

    Add files via upload

---
+++ konversi suhu.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	//DETA SARI ASLINA (2217051117)
+	
+	float celcius;
+    float reamur, fahreinhet, kelvin;
+    
+    cout<<"---------------------------------------------"<<endl;
+    cout<<"...........     KONVERSI SUHU    ............"<<endl;
+    cout<<endl;
+    
+    cout<<"masukan derajat celcius : ";
+    cin>>celcius;
+    
+    reamur = 4.0/5.0 * celcius;
+ 	fahreinhet = 9.0/5.0 * celcius + 32;212;
+ 	kelvin = celcius + 273;
+ 	
+ 	cout<<"Reamur : "<<reamur<<endl;
+	cout<<"Fahreinhet : "<<fahreinhet<<endl;
+ 	cout<<"Kelvin : "<<kelvin<<endl;
+
+return 0;
+}
